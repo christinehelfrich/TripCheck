1daec5b71bdeabd0f1627d2142b857befbb001ad
Christine <christine.helfrich1@gmail.com>
2024-07-24 12:12:12 -0700

    update home page

--- tc-frontend/src/components/pages/HomePage.js
+++ tc-frontend/src/components/pages/HomePage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import {useSelector} from "react-redux"
 const HomePage = () => {
 
@@ -15,10 +14,7 @@ const HomePage = () => {
         <h2>Hello, {user.user.name} </h2>
       )}
       {!user.isAuthenticated && (
-        <>
-        <Link className='homeLink' to={'/login'}><p>Log In</p></Link>
-        <Link className='homeLink' to={'/signup'}><p>Sign Up</p></Link>
-        </>
+        <h2>Hello, please create an account! </h2>
       )}
 
       

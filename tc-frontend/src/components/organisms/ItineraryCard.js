226f3ad5d07fdd2d3f98e492fecb76c4e8eb99c2
Christine <christine.helfrich1@gmail.com>
2024-07-28 11:29:28 -0700

    add image not found

--- tc-frontend/src/components/organisms/ItineraryCard.js
+++ tc-frontend/src/components/organisms/ItineraryCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../styles/ItineraryCard.css'
 import { useNavigate } from 'react-router-dom'
-const imageBasePath = '../../../../tc-backend'
+const imageBasePath = 'https://raw.githubusercontent.com/christinehelfrich/TripCheck/master/tc-backend/'
 
 const ItineraryCard = (itinerary) => {
   const navigate = useNavigate()
@@ -9,12 +9,12 @@ const ItineraryCard = (itinerary) => {
   const onClickCard = () => {
     navigate(`/itinerary/${itinerary.itinerary._id}`)
   }
-  console.log(itinerary)
+  console.log(imageBasePath + '/' + itinerary.itinerary.itineraryImage)
 
   return (
     <div className='itinerary-card' onClick={onClickCard}>
-    <img alt='img' src={imageBasePath + '/' + itinerary.itineraryImage}/>
-        <p>{itinerary.itinerary.itineraryName}</p>
+      <p>{itinerary.itinerary.itineraryName}</p>
+    <img className='itinerary-card-img' alt={itinerary.itinerary.itineraryName + '-cover-image'} src={imageBasePath + itinerary.itinerary.itineraryImage}/>
     </div>
   )
 }
--- tc-frontend/src/components/pages/MyTripsPage.js
+++ tc-frontend/src/components/pages/MyTripsPage.js
@@ -6,22 +6,32 @@ import '../../styles/MyTrips.css'
 const MyTripsPage = () => {
 
     const [itineraries, setItineraries] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
+        setIsLoading(true)
         fetchItineraries()
     }, [])
 
     const fetchItineraries = async () => {
         let res = await getAllItineraries();
         res?.data ? setItineraries(res.data) : setItineraries([])
+        setIsLoading(false)
     }
 
   return (
-    <div className='my-trips'>
+    <div >
 
+        {isLoading && (
+            <p>loading....</p>
+        )}
+    {!isLoading && (
+        <div className='my-trips'>
         {itineraries.map((itinerary, index) => (
             <ItineraryCard itinerary={itinerary}></ItineraryCard>
         ))}
+        </div>
+    )}
       
     </div>
   )

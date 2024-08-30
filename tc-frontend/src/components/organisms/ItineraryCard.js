162ff45d436e35ca4e74fca81af1cafdafbe1911
Christine <christine.helfrich1@gmail.com>
2024-08-30 13:12:52 -0700

    add lazy loading to images

--- tc-frontend/src/components/organisms/ItineraryCard.js
+++ tc-frontend/src/components/organisms/ItineraryCard.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../../styles/ItineraryCard.css'
 import { useNavigate } from 'react-router-dom'
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import 'react-lazy-load-image-component/src/effects/blur.css';
 const imageBasePath = 'https://raw.githubusercontent.com/christinehelfrich/TripCheck/master/tc-backend/'
 
 const ItineraryCard = (itinerary) => {
@@ -15,7 +17,11 @@ const ItineraryCard = (itinerary) => {
 
   return (
     <div className='itinerary-card' onClick={onClickCard}>
-    <img className='itinerary-card-img' alt={itinerary.itinerary.itineraryName + '-cover-image'} src={itinerary.itinerary.itineraryImage ? imageBasePath + itinerary.itinerary.itineraryImage : imageBasePath + 'images/itineraryImages/image-not-found.png'}/>
+    <LazyLoadImage 
+    effect="blur"
+    className='itinerary-card-img' 
+    alt={itinerary.itinerary.itineraryName + '-cover-image'} 
+    src={itinerary.itinerary.itineraryImage ? imageBasePath + itinerary.itinerary.itineraryImage : imageBasePath + 'images/itineraryImages/image-not-found.png'}/>
     <p>{itinerary.itinerary.itineraryName}
     {itinerary.itinerary.startDate !== undefined && (
       <span className='itinerary-card-date'>{startDate.toLocaleDateString()} - {endDate.toLocaleDateString()}</span>

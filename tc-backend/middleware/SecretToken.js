5a83f8d07751ba269e5cdf9e517443afa9dd34ec
Christine <christine.helfrich1@gmail.com>
2024-08-30 13:32:43 -0700

    remove env files

--- tc-backend/middleware/SecretToken.js
+++ tc-backend/middleware/SecretToken.js
@@ -2,7 +2,5 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 module.exports.createSecretToken = (id) => {
-    console.log('id', id)
-    console.log('process.env.TOKEN_KEY', process.env.TOKEN_KEY)
-    return jwt.sign({ id }, process.env.TOKEN_KEY, { expiresIn: "1d" });
+    return jwt.sign({ id }, process.env.TOKEN_KEY /*, { expiresIn: "1d" }*/);
 };
